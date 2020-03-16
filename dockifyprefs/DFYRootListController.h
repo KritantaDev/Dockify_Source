34c03d584d6523619fe2e4e8b35a8d2be4e285b8
KritantaDev <KritantaDevelopment@gmail.com>
2020-03-16 14:43:18 -0500

    .

--- dockifyprefs/DFYRootListController.h
+++ dockifyprefs/DFYRootListController.h
@@ -1,7 +1,7 @@
 #import <Preferences/PSListController.h>
-/*#import <Preferences/PSTableCell.h>
+#import <Preferences/PSTableCell.h>
 #import <Preferences/PSSpecifier.h>
-#import <CepheiPrefs/HBRootListController.h>
+/*#import <CepheiPrefs/HBRootListController.h>
 #import <CepheiPrefs/HBAppearanceSettings.h>
 #import <CepheiPrefs/HBPackageNameHeaderCell.h>
 #import <CepheiPrefs/HBLinkTableCell.h>
